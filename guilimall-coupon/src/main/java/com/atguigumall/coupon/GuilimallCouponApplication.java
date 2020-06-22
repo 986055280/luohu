391f378647a17bc140949ea70d6001f07d32315e
luohu <lh986055280@163.com>
2020-06-23 00:10:07 +0800

    项目初始结构创建

---
+++ guilimall-coupon/src/main/java/com/atguigumall/coupon/GuilimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.atguigumall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilimallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilimallCouponApplication.class, args);
+	}
+
+}
